7ec0eb628d3c141fdaec475a69f37f52ec749dec
Brandon DeRosier <x@bdero.me>
2021-09-03 15:14:20 -0700

    Initial template commit

---
+++ bin/rollerdash.dart
@@ -0,0 +1,5 @@
+import 'package:rollerdash/rollerdash.dart' as rollerdash;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rollerdash.calculate()}!');
+}
---
+++ test/rollerdash_test.dart
@@ -0,0 +1,8 @@
+import 'package:rollerdash/rollerdash.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

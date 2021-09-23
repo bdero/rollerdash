52ad1e16d2645ea3ef6c29649d8b4bd9b3725204
Brandon DeRosier <x@bdero.me>
2021-09-22 22:12:58 -0700

    HTTP request to grab roller data

--- lib/rollerdash.dart
+++ lib/rollerdash.dart
@@ -1,7 +1,36 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 import 'package:rollerdash/config.dart';
 
 class RollerSummary {}
 
-void printSummary(Config config) {}
+// curl -X POST https://autoroll.skia.org/twirp/autoroll.rpc.AutoRollService/GetStatus \
+//      -H 'Content-Type: application/json' -d '{"roller_id": "skia-flutter-autoroll"}'
+//
+// skia-flutter-autoroll
+// clang-linux-flutter-engine
+// clang-mac-flutter-engine
+// dart-sdk-flutter-engine
+// flutter-engine-flutter-autoroll
+// flutter-plugins-flutter-autoroll
+// fuchsia-linux-sdk-flutter-engine
+// fuchsia-mac-sdk-flutter-engine
+void printSummary(Config config) async {
+  var response = await http.post(
+      Uri.parse(
+          'https://autoroll.skia.org/twirp/autoroll.rpc.AutoRollService/GetStatus'),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: '{"roller_id": "skia-flutter-autoroll"}');
+
+  if (response.statusCode != 200) {
+    print('http request failed');
+    return;
+  }
+
+  Map<String, dynamic> result = jsonDecode(response.body);
+  List<Map<String, dynamic>> rolls = result['status']?['recent_rolls'];
+}

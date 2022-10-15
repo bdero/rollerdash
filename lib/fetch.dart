ed29c13b4bfad3c5df6eadb3be3da0ab1e02a764
Brandon DeRosier <bdero@google.com>
2022-10-14 17:08:30 -0700

    Retries with exponential backoff and jitter

--- lib/fetch.dart
+++ lib/fetch.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:http/http.dart' as http;
+import 'package:http/retry.dart';
 
 import 'package:rollerdash/schema.dart';
 
@@ -13,7 +15,29 @@ Future<List<StatusModel>> getAllStatuses(List<String> rollerIds) async {
 }
 
 Future<StatusModel> getStatus(String rollerId) async {
-  var response = await http.post(
+  // curl https://autoroll.skia.org/twirp/autoroll.rpc.AutoRollService/GetStatus
+  //      -X POST -H "Content-Type: application/json; charset=UTF-8"
+  //      -d '{"roller_id": "skia-flutter-autoroll"}'
+
+  // Autoroller service RPCs fail quite frequently, so we perform retries with
+  // exponential backoff and jitter.
+  var random = Random();
+  Duration delay(double seconds) {
+    var ms = seconds * 1000;
+    return Duration(
+        milliseconds: (ms + random.nextDouble() * ms * 0.3) ~/ 2);
+  }
+  var client = RetryClient.withDelays(http.Client(), [
+    delay(1),
+    delay(2),
+    delay(3),
+    delay(5),
+    delay(8),
+    delay(13),
+    delay(21),
+  ]);
+
+  var response = await client.post(
       Uri.parse(
           'https://autoroll.skia.org/twirp/autoroll.rpc.AutoRollService/GetStatus'),
       headers: {
@@ -23,7 +47,7 @@ Future<StatusModel> getStatus(String rollerId) async {
 
   if (response.statusCode != 200) {
     throw Exception(
-        "Failed to fetch `$rollerId` (status code: ${response.statusCode})");
+        "Failed to fetch `$rollerId` 7 times (status code: ${response.statusCode})");
   }
 
   Map<String, dynamic> result = jsonDecode(response.body);

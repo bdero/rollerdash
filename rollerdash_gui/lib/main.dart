8eb6ab6be138af9204bc6078bc208eb21693e15d
Brandon DeRosier <bdero@google.com>
2022-10-17 03:58:30 -0700

    Add all chip states

--- rollerdash_gui/lib/main.dart
+++ rollerdash_gui/lib/main.dart
@@ -196,6 +196,119 @@ class _MainPageState extends State<MainPage> {
   }
 }
 
+class RollerChip extends StatelessWidget {
+  const RollerChip(
+      {Key? key, required this.label, this.avatar, this.backgroundColor})
+      : super(key: key);
+
+  final Widget? avatar;
+  final Widget label;
+  final Color? backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Chip(
+      visualDensity: const VisualDensity(horizontal: 0, vertical: -4),
+      avatar: avatar,
+      label: label,
+      backgroundColor: backgroundColor,
+      side: BorderSide.none,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(100)),
+      ),
+    );
+  }
+}
+
+class RollStatusChip extends StatelessWidget {
+  const RollStatusChip({Key? key, required this.status}) : super(key: key);
+
+  final String status;
+
+  @override
+  Widget build(BuildContext context) {
+    String statusText;
+    Color backgroundColor;
+    Widget avatar;
+    switch (status) {
+      case 'IN_PROGRESS':
+        statusText = 'In Progress';
+        backgroundColor = Colors.yellow;
+        avatar = const SizedBox(
+          width: 10,
+          height: 10,
+          child: CircularProgressIndicator(
+            color: Colors.black,
+            strokeWidth: 3,
+          ),
+        );
+        break;
+      case 'SUCCESS':
+        statusText = 'Success';
+        backgroundColor = Colors.green;
+        avatar = const Icon(
+          Icons.check_outlined,
+          color: Colors.black,
+        );
+        break;
+      default: // FAILURE
+        statusText = 'Failure';
+        backgroundColor = Colors.red;
+        avatar = const Icon(
+          Icons.error_outline,
+          color: Colors.black,
+        );
+        break;
+    }
+
+    return RollerChip(
+      avatar: avatar,
+      label: Text(
+        statusText,
+        style: const TextStyle(fontSize: 12, color: Colors.black),
+      ),
+      backgroundColor: backgroundColor,
+    );
+  }
+}
+
+class RollModeChip extends StatelessWidget {
+  const RollModeChip({Key? key, required this.mode}) : super(key: key);
+
+  final String mode;
+
+  @override
+  Widget build(BuildContext context) {
+    String modeText;
+    Color backgroundColor;
+    IconData icon;
+    switch (mode) {
+      case 'RUNNING':
+        modeText = 'Running';
+        backgroundColor = Colors.green;
+        icon = Icons.rocket_launch_outlined;
+        break;
+      default: // STOPPED
+        modeText = 'Stopped';
+        backgroundColor = Colors.red;
+        icon = Icons.pause_outlined;
+        break;
+    }
+
+    return RollerChip(
+      avatar: Icon(
+        icon,
+        color: Colors.black,
+      ),
+      label: Text(
+        modeText,
+        style: const TextStyle(fontSize: 12, color: Colors.black),
+      ),
+      backgroundColor: backgroundColor,
+    );
+  }
+}
+
 class Roller extends StatelessWidget {
   const Roller({Key? key, required this.status}) : super(key: key);
 
@@ -224,64 +337,20 @@ class Roller extends StatelessWidget {
                     Text(status.mini_status.roller_id),
                     Wrap(
                       spacing: 10,
-                      children: const [
-                        Chip(
-                          visualDensity:
-                              VisualDensity(horizontal: 0, vertical: -4),
-                          avatar: SizedBox(
-                            width: 10,
-                            height: 10,
-                            child: CircularProgressIndicator(
-                              color: Colors.black,
-                              strokeWidth: 3,
-                            ),
-                          ),
-                          label: Text(
-                            "In Progress",
-                            style: TextStyle(fontSize: 12, color: Colors.black),
-                          ),
-                          backgroundColor: Colors.yellow,
-                          side: BorderSide.none,
-                          shape: RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(100)),
-                          ),
-                        ),
-                        Chip(
-                          visualDensity:
-                              VisualDensity(horizontal: 0, vertical: -4),
-                          avatar: Icon(
-                            Icons.rocket_launch_outlined,
-                            color: Colors.black,
-                          ),
-                          label: Text(
-                            "Running",
-                            style: TextStyle(fontSize: 12, color: Colors.black),
-                          ),
-                          backgroundColor: Colors.green,
-                          side: BorderSide.none,
-                          shape: RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(100)),
-                          ),
-                        ),
-                        Chip(
-                          visualDensity:
-                              VisualDensity(horizontal: 0, vertical: -4),
-                          avatar: Icon(
+                      children: [
+                        RollStatusChip(status: status.recent_rolls[0].result),
+                        RollModeChip(mode: status.mini_status.mode),
+                        RollerChip(
+                          avatar: const Icon(
                             Icons.commit_outlined,
                             color: Colors.black,
                           ),
                           label: Text(
-                            "Behind 3 commits",
-                            style: TextStyle(fontSize: 12, color: Colors.black),
+                            "Behind ${status.mini_status.num_behind} commit${status.mini_status.num_behind == 1 ? " " : "s"}",
+                            style: const TextStyle(
+                                fontSize: 12, color: Colors.black),
                           ),
                           backgroundColor: Colors.grey,
-                          side: BorderSide.none,
-                          shape: RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(100)),
-                          ),
                         ),
                       ],
                     ),
@@ -312,7 +381,7 @@ class Roller extends StatelessWidget {
               decoration: BoxDecoration(
                 border: Border(
                     top: BorderSide(
-                        width: 5,
+                        width: 2,
                         color: Theme.of(context).scaffoldBackgroundColor)),
               ),
               child: Column(
